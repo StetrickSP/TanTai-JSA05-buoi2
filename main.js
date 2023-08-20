a2edf00f43a054fc47e5ac8890f57a2ff7065a01
StetrickSP <141539538+StetrickSP@users.noreply.github.com>
2023-08-20 11:31:26 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// let myArray=[4,5,7,8,9,1];
+
+// alert(myArray);
+
+// // for(let i=0; i < myArray.length; i++)
+// // {
+// //     console.log( "Gia tri cua phan tu thu", i, "la" , myArray[1] )
+// // }
+
+// // myArray[2] = 6;
+// // console.log(myArray);
+
+// myArray.splice(2,1,9);
+// alert(myArray);
+
+
+// let dog =
+// {
+//     name: "Huy",
+//     age: 4
+// }
+// dog.name = "Minh";
+// console.log(dog);
+// dog.action = "walk";
+
+// let cat = new Object
+// ({
+//     name: "Thorfinn",
+//     age: 3,
+//     enemy: 
+//     {
+//         name: 'Askeladd',
+//         age: 5
+//     }
+// }
+// )
+// console.log(cat);
+
+// let dog1 = ["Askeladd",4];
+// let dog2 = ["Thorfinn",5];
+// let dogs = [dog1,dog2];
+
+// console.log(dogs);
+// console.log(dogs[0][1]);
+
+let fruits = 
+[
+    ["peaches", 4],
+    ["apples", 3]
+]
+
+for(var i = 0; i < fruits.length; i++)
+{
+    alert("Gio trai cay co:" + fruits[i][0] + fruits[i][1]);
+}
+
---
+++ script.js
@@ -0,0 +1,23 @@
+let traicay=["Grapes",15];
+let i=0;
+while(i<=1)
+{
+    console.log("I have " + traicay[1]+ " "+traicay[0]);
+    i++;
+
+}
+
+do{
+    console.log("I have"+" "+ traicay[1]+" "+traicay[0]);
+    i++;
+
+}while(i<=1)
+for(var i=0;i<=0;i++){
+    console.log("I have" + " " + traicay[1] + " " + traicay[0]);
+}
+var myDog={
+    "name":"huy",
+    "chân":"4",
+    "color":"brown"
+
+};
