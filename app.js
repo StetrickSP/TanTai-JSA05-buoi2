9135585416d102fb01f7bb0e736d478420f4bd0a
StetrickSP <141539538+StetrickSP@users.noreply.github.com>
2023-08-20 12:03:26 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+// let arr = ["Math", "Literature", "English"]
+
+// for (let i in arr)
+// {
+//     console.log(arr[i]);
+// }
+
+let obj = 
+{
+    'name' : "Vo Dinh Huy",
+    'sex' : "Unidentified",
+    'crime': "Simplord"
+}
+
+for (let i of obj)
+{
+    console.log(obj[i]);
+}
